e19f7d255f312f2375979d746574663efd3ba19a
Darren Solorzano <dsolor08@gmail.com>
2023-06-24 17:29:55 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import solorzano from './solorzano.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={solorzano} className="App-logo" alt="logo" />
+        <p>
+          Solorzano Quality Machine Works
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
